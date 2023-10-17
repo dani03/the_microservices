b04e3e5bd043ac8383c01970e899b91ec3bd9168
daniel <lesamessipa@gmail.com>
2023-10-17 16:32:16 +0200

    update dockerfile and add php container

--- car-parking/docker/entrypoint.sh
+++ car-parking/docker/entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+chmod +x entrypoint.sh
 
-if [!  -f "vendor/autoload.php"]; then 
+if [ !  -f "vendor/autoload.php" ]; then 
   composer install  --no-progress --no-interaction
 fi
 
-if [! ".env"]; then
-  
+if [ ! -f ".env" ]; then
+  echo "creation du fichier env $APP_ENV"
+  cp .env.exemple .env
+else
+  echo "le fichier env  exist deja "
+fi
+
+php artisan migrate 
+php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan optimize
+
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
